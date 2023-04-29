9f9e721fa1ec77afcedabfabcae1f89880c0c64e
gabiezzi <geiezzi90@gmail.com>
2023-04-28 23:56:27 -0300

    add structure model, repo, controller

---
+++ src/main/java/com/gabiezzi/fullstackbackend/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.gabiezzi.fullstackbackend.repository;
+
+import com.gabiezzi.fullstackbackend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
